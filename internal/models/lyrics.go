714da88cd83827aa2adac3cd64e2fb18e50e6c08
22fariz22 <fariz08@gmail.com>
2024-11-22 18:18:27 +0300

    add models for db

--- internal/models/lyrics.go
+++ internal/models/lyrics.go
@@ -1,14 +1,28 @@
 package models
 
-// request
-type Song struct {
-	Group string `json:"group"`
-	Song  string `json:"song"`
+import "time"
+
+// SongRequest для добавления песни
+type SongRequest struct {
+	Group string `json:"group" validate:"required"`
+	Song  string `json:"song" validate:"required"`
 }
 
-// response
+// SongDetail для ответа
 type SongDetail struct {
 	ReleaseDate string `json:"releaseDate"`
 	Text        string `json:"text"`
 	Link        string `json:"link"`
+}
+
+// Song модель базы данных
+type Song struct {
+	ID          uint   `gorm:"primaryKey"`
+	GroupName   string `gorm:"type:varchar(255);not null"`
+	SongName    string `gorm:"type:varchar(255);not null"`
+	ReleaseDate *time.Time
+	Text        []string `gorm:"type:text[]"`
+	Link        *string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 }
