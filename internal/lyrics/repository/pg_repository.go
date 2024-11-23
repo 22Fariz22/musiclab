bdec5acec5cf324395843830c9fe9165061dc5e9
22fariz22 <fariz08@gmail.com>
2024-11-23 23:01:44 +0300

    add UpdateTrackByID

--- internal/lyrics/repository/pg_repository.go
+++ internal/lyrics/repository/pg_repository.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 
 	"github.com/22Fariz22/musiclab/internal/lyrics"
+	"github.com/22Fariz22/musiclab/internal/models"
 	"github.com/22Fariz22/musiclab/pkg/logger"
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
@@ -52,5 +53,49 @@ func (r lyricsRepo) DeleteSongByGroupAndTrack(ctx context.Context, groupName str
 		return errors.Wrap(sql.ErrNoRows, "SongRepository.DeleteSongByGroupAndName.rowsAffected")
 	}
 
+	return nil
+}
+
+func (r lyricsRepo)UpdateTrackByID(ctx context.Context, updateData models.UpdateTrackRequest) error{
+	r.logger.Debugf("in repo UpdateTrackByID() updateData:%d", updateData)
+
+	query := `
+	UPDATE songs
+	SET 
+		group_name = COALESCE($1, group_name),
+		song_name = COALESCE($2, song_name),
+		release_date = COALESCE($3, release_date),
+		text = COALESCE($4, text),
+		link = COALESCE($5, link),
+		updated_at = NOW()
+		WHERE id = $6
+		`
+		
+	result, err := r.db.ExecContext(
+		ctx,
+		query,
+		updateData.GroupName,  
+		updateData.SongName,   
+		updateData.ReleaseDate, 
+		updateData.Text,        
+		updateData.Link,        
+		updateData.ID,                    
+	)
+	if err != nil {
+		r.logger.Debugf("in repo UpdateTrackByID() r.db.ExecContext return error: ",err)
+		return errors.Wrap(err, "LyricsRepository.UpdateTrackByID.ExecContext")
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		r.logger.Debugf("in repo UpdateTrackByID() result.RowsAffected() return error: ",err)
+		return errors.Wrap(err, "LyricsRepository.UpdateTrackByID.RowsAffected")
+	}
+	if rowsAffected == 0 {
+		r.logger.Debug("in repo UpdateTrackByID() rowsAffected == 0")
+		return errors.Wrap(sql.ErrNoRows, "LyricsRepository.UpdateTrackByID.rowsAffected")
+	}
+
+	r.logger.Debug("in repo UpdateTrackByID() return nil")
 	return nil
 }
