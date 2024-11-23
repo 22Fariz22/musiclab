d7bca446e5d5169e7c432140b13271effae33977
22fariz22 <fariz08@gmail.com>
2024-11-23 17:53:44 +0300

    add delete by song and group

--- internal/lyrics/repository/pg_repository.go
+++ internal/lyrics/repository/pg_repository.go
@@ -1,9 +1,13 @@
 package repository
 
 import (
+	"context"
+	"database/sql"
+
 	"github.com/22Fariz22/musiclab/internal/lyrics"
 	"github.com/22Fariz22/musiclab/pkg/logger"
 	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
 )
 
 type lyricsRepo struct {
@@ -24,5 +28,29 @@ func (r lyricsRepo) Ping() error {
 		return err
 	}
 	r.logger.Debug("Pong in repo Ping()")
+	return nil
+}
+
+func (r lyricsRepo) DeleteSongByGroupAndTrack(ctx context.Context, groupName string, trackName string) error {
+	r.logger.Debugf("In repo. Deleting song. Group: %s, Track: %s", groupName, trackName)
+
+	query := "DELETE FROM songs WHERE group_name = $1 AND song_name = $2"
+
+	result, err := r.db.ExecContext(ctx, query, groupName, trackName)
+	if err != nil {
+		r.logger.Debugf("error in repo r.db.ExecContext(): ", err)
+		return errors.Wrap(err, "SongRepository.DeleteSongByGroupAndTrack.ExecContext")
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		r.logger.Debugf("error in repo result.RowsAffected(): ", err)
+		return errors.Wrap(err, "SongRepository.DeleteSongByGroupAndName.RowsAffected")
+	}
+	if rowsAffected == 0 {
+		r.logger.Debugf("in repo rowsAffected == 0")
+		return errors.Wrap(sql.ErrNoRows, "SongRepository.DeleteSongByGroupAndName.rowsAffected")
+	}
+
 	return nil
 }
