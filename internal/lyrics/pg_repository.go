672b39a2fa9ccc1eeca1db4e2ee34bd9d28e6cfa
22fariz22 <fariz08@gmail.com>
2024-11-25 22:24:13 +0300

    delete comment

--- internal/lyrics/pg_repository.go
+++ internal/lyrics/pg_repository.go
@@ -11,6 +11,5 @@ type Repository interface {
 	DeleteSongByGroupAndTrack(ctx context.Context, groupName string, trackName string) error
 	UpdateTrackByID(ctx context.Context, updateData models.UpdateTrackRequest) error
 	CreateTrack(ctx context.Context, song models.SongRequest, songDetail models.SongDetail) error
-	// GetVerses(ctx context.Context, ID uint) error
 	GetSongByID(ctx context.Context, id uint) (models.Song, error)
 }
